04ae85e11297023d57e68fa7f8752f7ecea70a35
abdulrd98 <sisteminformasi98@gmail.com>
2024-05-24 15:40:45 +0700

    update file index.php

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <title>Coba Git Remote</title>
   </head>
   <body>
+    php
     <h1>hello world</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, aliquid ipsa. Dolor quis ipsam et quaerat rerum soluta ipsa quia. Nam nulla dolorum doloremque quaerat, consectetur maiores iste facilis possimus!</p>
   </body>
 </html>
